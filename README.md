6113de7455cc67cc63923ee51f57acbff5d56936
魏天尧 <tywei90@gmail.com>
2018-02-09 02:55:05 +0800

    第一版完成

---
+++ README.md
@@ -0,0 +1,60 @@
+## Install
+
+Install with [npm](https://www.npmjs.com/):
+
+```sh
+$ npm install --save arr-del
+```
+
+Install with [yarn](https://yarnpkg.com):
+
+```sh
+$ yarn add arr-del
+```
+
+## Usage
+
+delete array elements by their indexes:
+
+```js
+var arrDel = require('arr-del');
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var out = arrDel(arr, [1, 3, 4, 7]);
+console.log(out);
+//=> [0, 2, 5, 6, 8, 9]
+```
+
+
+## Params
+
+```js
+arrDel(array, indexArr);
+```
+
+* `array`: **{Array}** The array to delete
+* `indexArr`: **{Number Array}**: Array consists of indexes which you want to delete
+
+## Note
+
+* You can still use -1 represent the last element of Array, and so on.
+* If `Math.abs(indexArr[i]) > array.length`, indexArr[i] will be ignore
+* You don't need to sort indexArr
+* The method 'arrDel' would not change the value of original array. So if you want to change it, keep in mind to assign to it.
+
+## Examples
+
+```js
+var arrDel = require('arr-del');
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var out = arrDel(arr, [1, 5, 3, -1, -2, 16, 20, -11, -23]);
+console.log(out);
+//=> [0, 2, 4, 6, 7]
+console.log(arr);
+//=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+```
+
+
+## License
+
+Copyright © 2018, [tywei90](https://github.com/tywei90).
+Released under the [MIT License](LICENSE).
---
+++ index.js
@@ -0,0 +1,61 @@
+/*
+ * arr-del <https://github.com/tywei90/arr-del>
+ *
+ * Copyright (c) 2018-2020, tywei90.
+ * Released under the MIT License.
+ */
+
+'use strict';
+
+
+
+/**
+ * Delete array elements in one time by array consists of their indexes
+ *
+ * @param  {Array} `arr` The Array to sort.
+ * @param  {Number Array} `indexArr` Array consists of indexes which you want to delete.
+ * @return {Array} Returns a new deleted array.
+ * @api public
+ */
+
+function arrDel(arr, indexArr) {
+	// check params
+	if(indexArr === undefined){
+		return arr
+	}
+	if(Object.prototype.toString.call(indexArr) !== "[object Array]"){
+		throw new TypeError('PARAM MUST BE ARRAY');
+	}
+	for(var i=0, len=indexArr.length; i < len; i++){
+		if(typeof indexArr[i] !== "number"){
+			throw new TypeError('PARAM MUST BE NUMBER ARRAY');
+		}
+		if(indexArr[i] < -indexArr.length){
+			indexArr[i] = 0;
+		}
+		if(indexArr[i] >= -indexArr.length && indexArr[i] < 0){
+			indexArr[i] = indexArr[i] + indexArr.length;
+		}
+	}
+	// first sort indexArr, then remove redupliction
+	indexArr.sort(function(a, b){
+		return a - b
+	})
+	var tmpArr = [];
+	for(var i=0, len=indexArr.length; i < len; i++){
+		if(tmpArr.indexOf(indexArr[i]) == -1){
+			tmpArr.push(indexArr[i])
+		}
+	}
+	// should not change the value of input arr
+	var outArr = JSON.parse(JSON.stringify(arr));
+	if (arr.length === 0) {
+		return [];
+	}
+	for (var i = 0, len = tmpArr.length; i < len; i++) {
+		outArr.splice(tmpArr[i] - i, 1);
+	}
+	return outArr
+}
+
+module.exports = arrDel;
